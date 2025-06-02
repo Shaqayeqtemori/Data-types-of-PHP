42972a95985273f20bbe35985c90489a32612e2b
Shaqayeq Temori <edristemory33@gmail.com>
2025-06-02 10:45:41 +0430

    data types

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="squre">
+        <!-- PHP codes -->
+        <?php
+            $name="Shaqayeq";
+            $age=19;
+            $time=10.5;
+            echo "Hi everyone!<br> My name is $name and I am $age.<br>
+            I started Coding from 2024 and about $time months I am working with this. ";
+        ?> 
+    </div>
+</body>
+</html>
